27c7dfcc197cc4c80c6d49e3f0d83d2224fe202b
nixjke <aziz.10.255@gmail.com>
2022-10-29 19:15:32 +0300

    init chart.js

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,11 @@ import React from 'react'
 
 import s from './App.module.scss'
 
-import { useDate } from 'helpers/getDate'
+import 'chart.js/auto'
+import { Bar } from 'react-chartjs-2'
 
 import { Header, Layout, SelectTime } from 'components'
+import { useDate } from 'helpers/getDate'
 
 function App() {
   const [timeType, setTimeType] = React.useState('15M')
@@ -23,6 +25,7 @@ function App() {
     <div style={{ padding: '30px' }}>
       <Layout>
         <Header name="BTC/USDT Price Chart" date={time} className={s.header} />
+        <Bar data={{ labels: ['Red', 'Blue','2'], datasets: [{ data: [12, 19, 12, 123, 123, 51] }] }} />
         <SelectTime value={timeType} onClick={handleClick} />
       </Layout>
     </div>
